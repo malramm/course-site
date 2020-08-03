ef04618234f716e5297cd5c7945770cb0a8e83b9
malramm <malcolm.ramm@optusnet.com.au>
2020-08-03 12:25:03 +1000

    added headers into callback of cloud function

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -28,11 +28,13 @@ exports.handler = function(event, context, callback) {
     if (body.password == "javascript") {
       callback(null, {
         statusCode: 200,
+        headers,
         body: secretContent
       })
     } else {
       callback(null, {
-        statusCode: 401
+        statusCode: 401,
+        headers
       })
     }
   
